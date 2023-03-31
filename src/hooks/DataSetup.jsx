b0e21a4f2555deecc99f8a9fb553f1edf11539db
Vegard Lekve <Vegard Lekve>
2023-03-31 17:07:07 +0200

    Fetching data from api

---
+++ src/hooks/DataSetup.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+export default function DataSetup(url) {
+    const [data, setData] = useState(null);
+    const [isLoading, SetIsLoading] = useState(false);
+    const [isError, SetIsError] = useState(false);
+
+    useEffect(() => {
+        async function getData() {
+            try {
+                SetIsLoading(true);
+                SetIsError(false);
+                const fetchedData = await fetch(url);
+                const json = await fetchedData.json();
+                setData(json);
+            } catch (error) {
+                console.log(error);
+                SetIsError(true);
+            } finally {
+                SetIsLoading(false);
+            }
+        }
+
+        getData();
+    }, [url]);
+
+    return { data, isLoading, isError };
+}
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+//Hooks
+import DataSetup from "../../hooks/DataSetup";
 //Styles
 import styled from "styled-components";
 
 export default function Home() {
+    const { data, isLoading, error } = DataSetup(
+        "https://api.noroff.dev/api/v1/online-shop"
+    );
+    if (isLoading) {
+        return <div>Loading</div>;
+    }
+
+    if (error) {
+        return <div>Error</div>;
+    }
+
+    console.log(data);
     return (
         <main>
             <div>
